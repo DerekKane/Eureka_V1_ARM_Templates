d4e777af2bdb5777d52e2a514b19bf541c1500be
Derek Kane <derek.m.kane@gmail.com>
2018-12-06 15:20:31 -0600

    cleaning up bad files

--- Azure_Powershell/Master.ps1
+++
@@ -1,6 +0,0 @@
-New-AzureRmDeployment `
-  -Name demoEmptyRG `
-  -Location southcentralus `
-  -TemplateUri https://raw.githubusercontent.com/Azure/azure-docs-json-samples/master/azure-resource-manager/emptyRG.json `
-  -rgName demogroup `
-  -rgLocation northcentralus
